52c44005ea1711ff0ce9419356e44e0b079f7d79
Luke Conley <luke.sovern.conley@gmail.com>
2018-04-05 14:50:22 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,6 +4,8 @@ if position_taken?(board, index) == true
   return false
 if index.between?(0,8) == false
   return false
+if index.between(0,8) == true && position_taken?(board,index) == false
+  return true
 end
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
