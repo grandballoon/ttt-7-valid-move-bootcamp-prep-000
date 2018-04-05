f1f931660f76a0dfebe306e46ff35b5c7286d244
Luke Conley <luke.sovern.conley@gmail.com>
2018-04-05 15:00:15 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 def valid_move?(board, index)
 if position_taken?(board, index) == true
   return false
-if index.between?(0,8) == false
+elsif index.between?(0,8) == false
   return false
 elsif index.between(0,8) == true && position_taken?(board,index) == false
   return true
